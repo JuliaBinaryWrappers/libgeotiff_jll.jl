3551c5c5bfea5d77fc57fa4ce6dd9686787ee5d1
jlbuild <juliabuildbot@gmail.com>
2022-01-07 17:13:44 +0000

    libgeotiff_jll build 1.7.0+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -3,14 +3,21 @@ export applygeo, geotifcp, libgeotiff, listgeo, makegeo
 
 using PROJ_jll
 using Libtiff_jll
+using LibCURL_jll
 JLLWrappers.@generate_wrapper_header("libgeotiff")
+JLLWrappers.@declare_library_product(libgeotiff, "libgeotiff.dll")
 JLLWrappers.@declare_executable_product(applygeo)
 JLLWrappers.@declare_executable_product(geotifcp)
-JLLWrappers.@declare_library_product(libgeotiff, "libgeotiff.dll")
 JLLWrappers.@declare_executable_product(listgeo)
 JLLWrappers.@declare_executable_product(makegeo)
 function __init__()
-    JLLWrappers.@generate_init_header(PROJ_jll, Libtiff_jll)
+    JLLWrappers.@generate_init_header(PROJ_jll, Libtiff_jll, LibCURL_jll)
+    JLLWrappers.@init_library_product(
+        libgeotiff,
+        "bin\\libgeotiff.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
     JLLWrappers.@init_executable_product(
         applygeo,
         "bin\\applygeo.exe",
@@ -19,12 +26,6 @@ function __init__()
     JLLWrappers.@init_executable_product(
         geotifcp,
         "bin\\geotifcp.exe",
-    )
-
-    JLLWrappers.@init_library_product(
-        libgeotiff,
-        "bin\\libgeotiff.dll",
-        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_executable_product(
